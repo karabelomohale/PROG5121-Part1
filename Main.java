bcb8fecf34765958ccc18f113589e7ef55d93674
karabelomohale <162301800+karabelomohale@users.noreply.github.com>
2025-04-14 21:15:03 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package poe_part1.pkg3;
+package poe.part1;
 import javax.swing.*;
 /**
  *
